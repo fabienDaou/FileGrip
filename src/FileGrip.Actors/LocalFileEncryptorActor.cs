23a913a69ce8aa4948ca7f67839b0490374aff19
Hycariss <fabien.daoulas@gmail.com>
2021-03-07 02:09:29 +0100

    Create pool of actors to encrypt files.

---
+++ src/FileGrip.Actors/LocalFileEncryptorActor.cs
@@ -0,0 +1,122 @@
+﻿using Akka.Actor;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileGrip.Actors
+{
+    public class LocalFileEncryptorActor : ReceiveActor
+    {
+        private readonly string _authorizedWorkingDirectory;
+        private readonly string _outputDirectory;
+
+        public LocalFileEncryptorActor(string authorizedWorkingDirectory, string outputDirectory)
+        {
+            if (authorizedWorkingDirectory is null)
+            {
+                throw new ArgumentNullException(nameof(authorizedWorkingDirectory));
+            }
+
+            _authorizedWorkingDirectory = Path.GetFullPath(authorizedWorkingDirectory + Path.DirectorySeparatorChar);
+            if (!Directory.Exists(_authorizedWorkingDirectory))
+            {
+                throw new ArgumentException($"{nameof(authorizedWorkingDirectory)} should point to an existing directory.");
+            }
+            _outputDirectory = outputDirectory ?? throw new ArgumentNullException(nameof(outputDirectory));
+            if (!Directory.Exists(outputDirectory))
+            {
+                throw new ArgumentException($"{nameof(outputDirectory)} should point to an existing directory.");
+            }
+
+            Receive<Encrypt>(Handle);
+
+            Console.WriteLine($"{nameof(LocalFileEncryptorActor)} started!");
+        }
+
+        private void Handle(Encrypt request)
+        {
+            Console.WriteLine($"Message received for file {request.RelativeFilePath}.");
+            var absoluteFilePath = Path.GetFullPath(Path.Combine(_authorizedWorkingDirectory, request.RelativeFilePath));
+            if (!absoluteFilePath.StartsWith(_authorizedWorkingDirectory, StringComparison.Ordinal))
+            {
+                Sender.Tell(new OutsideOfWorkingDirectory(request.RelativeFilePath));
+                return;
+            }
+            else if (!File.Exists(absoluteFilePath))
+            {
+                Sender.Tell(new FileDoesNotExist(request.RelativeFilePath));
+                return;
+            }
+
+            using var aes = Aes.Create();
+            aes.Key = request.Key;
+
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using var outputFileStream = File.Create(Path.Combine(_outputDirectory, Path.GetFileName(absoluteFilePath)));
+            using var cryptoStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
+            using var streamWriter = new BinaryWriter(cryptoStream);
+            using var inputFileStream = File.Open(absoluteFilePath, FileMode.Open, FileAccess.Read, FileShare.None);
+
+            var fileLength = inputFileStream.Length;
+            var buffer = new byte[1024];
+            while (true)
+            {
+                var lengthRead = inputFileStream.Read(buffer, 0, buffer.Length);
+                if (lengthRead == 0)
+                {
+                    break;
+                }
+                streamWriter.Write(buffer.AsSpan(0, lengthRead));
+            }
+
+            Sender.Tell(new Success(aes.IV));
+            Console.WriteLine($"Done encrypting {request.RelativeFilePath}.");
+
+            return;
+        }
+
+        public class Encrypt
+        {
+            public Encrypt(string filePath, byte[] key)
+            {
+                RelativeFilePath = filePath;
+                Key = key;
+            }
+
+            public string RelativeFilePath { get; }
+
+            public byte[] Key { get; }
+        }
+
+        public class Success
+        {
+            public Success(byte[] iV)
+            {
+                IV = iV;
+            }
+
+            public byte[] IV { get; }
+        }
+
+        public class FileDoesNotExist
+        {
+            public string FilePath { get; }
+
+            public FileDoesNotExist(string filePath)
+            {
+                FilePath = filePath;
+            }
+        }
+
+        public class OutsideOfWorkingDirectory
+        {
+            public string FilePath { get; }
+
+            public OutsideOfWorkingDirectory(string filePath)
+            {
+                FilePath = filePath;
+            }
+        }
+    }
+}
---
+++ src/FileGrip.Cryptography.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using FileGrip.Actors;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileGrip.Cryptography.Console
+{
+    class Program
+    {
+        private const string workspacePath = "C:\\Users\\Hycariss\\Documents\\Dev\\workspace";
+        private const string outputPath = "C:\\Users\\Hycariss\\Documents\\Dev\\output";
+        private static ActorSystem actorSystem;
+        static void Main(string[] args)
+        {
+            actorSystem = ActorSystem.Create("ActorSystem");
+
+            // todo
+            // change output file name by a guid
+            // get back IV and store it in a meta file {guid}.meta.json
+            // { "iv": ..., "encryptedFilename": "..." }
+
+            // todo
+            // create decrypt actor restoring content and filenames
+
+            // Start a pool of encryptor actor to parallelize work.
+            var props = Props.Create<LocalFileEncryptorActor>(workspacePath, outputPath).WithRouter(new RoundRobinPool(5));
+            var actorRef = actorSystem.ActorOf(props, "router");
+
+            using var aes = Aes.Create();
+            System.Console.WriteLine($"Encrypting files with key '{string.Join(", ", aes.Key)}'");
+            foreach (var file in Directory.GetFiles(workspacePath))
+            {
+                actorRef.Tell(new LocalFileEncryptorActor.Encrypt(file, aes.Key));
+            }
+
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+}
